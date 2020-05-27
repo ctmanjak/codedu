d3c1df60a6cf98653361fa96d57d0e0850a97e9f
ctmanjak <ctmanjak@gmail.com>
2020-05-27 16:07:14 +0900

    test cd

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -2,8 +2,8 @@
 
 TARGET_IP=$1
 
-docker build -t ${TARGET_IP}:5000/codedu_server:falcon -f Dockerfile-falcon .
+docker build -t ctmanjak/codedu_server:falcon -f Dockerfile-falcon .
 
-docker push ${TARGET_IP}:5000/codedu_server:falcon
+docker push ctmanjak/codedu_server:falcon
 
 DOCKER_HOST=tcp://${TARGET_IP}:2375 docker stack deploy -c docker-compose.yml codedu
