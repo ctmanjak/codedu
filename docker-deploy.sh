8a61def644388b1f8d26e7b5956a364d10c00701
ctmanjak <ctmanjak@gmail.com>
2020-05-27 15:41:21 +0900

    test travis ci

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+
+TARGET_DEPLOY_TCP=$1
+
 docker build -t 15.165.181.246:5000/codedu_server:falcon -f Dockerfile-falcon .
 
 docker push 15.165.181.246:5000/codedu_server:falcon
+
+DOCKER_HOST=${TARGET_DEPLOY_TCP} docker stack deploy -c docker-compose.yml codedu
