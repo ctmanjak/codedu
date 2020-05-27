a30d2bc052b01f86b02bcaa12b7bd303bfb73e47
ctmanjak <ctmanjak@gmail.com>
2020-05-27 23:47:39 +0900

    test cd

--- image_build.sh
+++ image_build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-if [ $1 ] && [ $DOCKER_PASSWORD ] && [ $DOCKER_USERNAME ]; then
-    TAG=$1
-    
+if [ $TRAVIS_BRANCH == "dev" ]; then
+    TAG="dev"
+elif [ $TRAVIS_BRANCH == "master" ]; then
+    TAG="latest"
+fi
+
+if [ $TAG ] && [ $DOCKER_PASSWORD ] && [ $DOCKER_USERNAME ]; then
     echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
     docker build -t "ctmanjak/codedu_server:${TAG}" -f Dockerfile-falcon .
 
     docker push "ctmanjak/codedu_server:${TAG}"
-else
-    exit 1
 fi
