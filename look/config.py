87e8a41d460e54cbe74d2ce62e8d823be2f3239c
ctmanjak <ctmanjak@gmail.com>
2020-05-27 14:10:47 +0900

    test travis ci

--- look/config.py
+++ look/config.py
@@ -1,7 +1,9 @@
+import os
+
 class Config:
-    secret = 'codedu'
-    DB_USER = 'root'
-    DB_PASSWORD = 'root'
-    DB_HOST = 'localhost'
-    DB_PORT = '3306'
-    DB_NAME = 'codedu'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'codedu')
+    DB_USER = os.getenv('DB_USER', 'root')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'root')
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_PORT = os.getenv('DB_PORT', '3306')
+    DB_NAME = os.getenv('DB_NAME', 'codedu')
