9ae1500ac29056a56e92b04940cc50a21e5ff123
ctmanjak <ctmanjak@gmail.com>
2020-05-27 14:23:28 +0900

    test travis ci

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,3 +1,5 @@
+sudo echo '{ "insecure-registries":["15.165.181.246:5000"] }' >> /etc/docker/daemon.json
+
 docker build -t 15.165.181.246:5000/codedu_server:falcon -f Dockerfile-falcon .
 
 # docker tag outsideris/demo:0.0.1 outsideris/demo:latest
