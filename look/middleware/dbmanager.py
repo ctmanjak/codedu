ce585cb45ab97d2a271d350d01addc91e5c4bca8
ctmanjak <ctmanjak@gmail.com>
2020-05-28 21:00:49 +0900

    test db session close

--- look/middleware/dbmanager.py
+++ look/middleware/dbmanager.py
@@ -3,14 +3,10 @@ class DBManager:
         self.db_session = db_session
 
     async def process_resource(self, req, resp, resource, params):
-        if req.method == 'OPTIONS':
-            return
         req.context['db_session'] = self.db_session()
         
     async def process_response(self, req, resp, resource, req_succeeded):
-        if req.method == 'OPTIONS':
-            return
-        if req.context.get('db_session'):
+        if "db_session" in req.context:
             if not req_succeeded:
                 req.context['db_session'].rollback()
             req.context['db_session'].close()
