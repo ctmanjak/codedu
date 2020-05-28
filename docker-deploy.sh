4c791e2a45eb4935cb78c652cd170ef9ef17623c
ctmanjak <ctmanjak@gmail.com>
2020-05-28 14:41:03 +0900

    test cd

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -7,8 +7,8 @@ fi
 
 if nc -z -w5 $TARGET_IP 2375; then
     MARIADB_CONF_VER=$(md5sum ./config/mariadb-default.cnf | awk '{print $1}')
-    NGINX_CONF_VER=$(md5sum ./config/nginx-default.cnf | awk '{print $1}')
-    
+    NGINX_CONF_VER=$(md5sum ./config/nginx-default.conf | awk '{print $1}')
+
     DOCKER_HOST=tcp://${TARGET_IP}:2375 \
     MARIADB_CONF_VER=$MARIADB_CONF_VER \
     NGINX_CONF_VER=$NGINX_CONF_VER \
