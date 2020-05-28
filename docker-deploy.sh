8f18184ca9e094c6537251dede0bf7da0fe0061f
ctmanjak <ctmanjak@gmail.com>
2020-05-28 18:12:57 +0900

    test cd

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -11,20 +11,31 @@ if nc -z -w5 $TARGET_IP 2375; then
     export NGINX_CONF_VER=$(md5sum ./config/nginx_default.conf | awk '{print $1}')
 
     if docker stack ls | grep -wq "codedu"; then
-        if ! docker config ls | grep -wq "mariadb_conf-${MARIADB_CONF_VER}"; then
-            PREV_MARIADB_CONF=$(docker service inspect codedu_mariadb | grep -oP '\"ConfigName\": "\K[^"]+' | uniq)
+        PREV_MARIADB_CONF=$(docker service inspect codedu_mariadb | grep -m1 -oP '\"ConfigName\": "\K[^"]+' | uniq)
+        PREV_NGINX_CONF=$(docker service inspect codedu_nginx | grep -m1 -oP '\"ConfigName\": "\K[^"]+' | uniq)
+        CURRENT_MARIADB_CONF="mariadb_conf-${MARIADB_CONF_VER}"
+        CURRENT_NGINX_CONF="nginx_conf-${NGINX_CONF_VER}"
 
-            docker config create "mariadb_conf-${MARIADB_CONF_VER}" ./config/mariadb_default.cnf
-            docker service update --config-add "mariadb_conf-${MARIADB_CONF_VER}" --config-rm $PREV_MARIADB_CONF codedu_mariadb
+        if [ ! $PREV_MARIADB_CONF = $CURRENT_MARIADB_CONF ]; then
+            if ! docker config ls | grep -wq $CURRENT_MARIADB_CONF; then
+                docker config create $CURRENT_MARIADB_CONF ./config/mariadb_default.cnf
+            fi
+            docker service update \
+                --config-add $CURRENT_MARIADB_CONF \
+                --config-rm $PREV_MARIADB_CONF \
+                codedu_mariadb
         fi
 
         docker service update codedu_falcon
 
-        if ! docker config ls | grep -wq "nginx_conf-${NGINX_CONF_VER}"; then
-            PREV_NGINX_CONF=$(docker service inspect codedu_nginx | grep -oP '\"ConfigName\": "\K[^"]+' | uniq)
-
-            docker config create "nginx_conf-${NGINX_CONF_VER}" ./config/nginx_default.conf
-            docker service update --config-add "nginx_conf-${NGINX_CONF_VER}" --config-rm $PREV_NGINX_CONF codedu_nginx
+        if [ ! $PREV_NGINX_CONF = $CURRENT_NGINX_CONF ]; then
+            if ! docker config ls | grep -wq $CURRENT_NGINX_CONF; then
+                docker config create $CURRENT_NGINX_CONF ./config/nginx_default.cnf
+            fi
+            docker service update \
+                --config-add $CURRENT_NGINX_CONF \
+                --config-rm $PREV_NGINX_CONF \
+                codedu_nginx
         fi
     else
         docker stack deploy -c ./docker/docker-compose.yml codedu
