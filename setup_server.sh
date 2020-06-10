c9846ad22afadbbe57d73caa5e3b02aa2418d3ce
ctmanjak <ctmanjak@gmail.com>
2020-06-10 16:03:22 +0900

    test

--- setup_server.sh
+++ setup_server.sh
@@ -6,16 +6,20 @@ apt-get install nfs-common -y
 
 if [ -d /etc/modules-load.d ]; then
     if [ -f /etc.modules-load.d/modules.conf ]; then
-        echo "nfs" >> /etc/modules-load.d/modules.conf
+        {
+            echo "nfs";
+            echo "nfsd";
+        } >> /etc/modules-load.d/modules.conf
     fi
 fi
+systemctl restart systemd-modules-load
 
 tmp_file="$HOME/docker-override.conf"
 
 {
     echo "[Service]"; 
     echo "ExecStart=";
-    echo "ExecStart=/usr/bin/dockerd"
+    echo "ExecStart=/usr/bin/dockerd";
 } >$tmp_file
 
 SYSTEMD_EDITOR="cp $tmp_file" systemctl edit docker
---
+++ setup_worker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sh install_docker.sh
+
+apt-get install nfs-common -y
