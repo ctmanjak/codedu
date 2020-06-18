647ce0d26535656538c8bdecdb2255d480c43631
ctmanjak <ctmanjak@gmail.com>
2020-06-19 03:10:57 +0900

    added graphql code schema

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -38,7 +38,7 @@ if ! docker ps | grep -wq nfs_server; then
         if ! docker ps | grep -wq nfs_server; then
             docker rm -f nfs_server
             echo "creating nfs_server"
-            docker run -d -v nfs_image:/codedu/images -e NFS_EXPORT_0='/codedu/images *(rw,no_root_squash,no_subtree_check)' -e NFS_EXPORT_0='/codedu/codes *(rw,no_root_squash,no_subtree_check)' \
+            docker run -d -v nfs_image:/codedu/images -v nfs_code:/codedu/codes -e NFS_EXPORT_0='/codedu/images *(rw,no_root_squash,no_subtree_check)' -e NFS_EXPORT_1='/codedu/codes *(rw,no_root_squash,no_subtree_check)' \
                 --privileged --name nfs_server -p 2049:2049 \
                 erichough/nfs-server
         fi
